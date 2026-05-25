8576bd13072877d2d0888076589e59e551ce8773
Nora Priour-Petitbon <nora.pp@orange.fr>
2026-05-25 10:08:36 +0200

    chore: init springboot

---
+++ backend/src/main/java/org/github/norapriour/clicktargetgame/ClickTargetGameApplication.java
@@ -0,0 +1,13 @@
+package org.github.norapriour.clicktargetgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickTargetGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickTargetGameApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/github/norapriour/clicktargetgame/ClickTargetGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.norapriour.clicktargetgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickTargetGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
